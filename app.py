b50e1448c879dccb330ee1c8848fdbe6423fbc51
Martin Neloe <Martin.A.Neloe@student.edu.au>
2024-03-17 16:37:12 +1100

    Test class changes

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 import tkinter as tk
 
+class StatusBar(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.label = tk.Label(self, bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        self.label.pack(side=tk.BOTTOM, fill=tk.X)
+    
+    def set(self, text):
+        self.label.config(text=text)
+        self.label.update_idletasks()
+
+    def clear(self):
+        self.label.config(text="")
+        self.label.update_idletasks()
+
+
 class MainApp(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
         self.parent = parent
+        self.parent.geometry("400x300")
+
+        self.statusbar = StatusBar(parent)
+        self.statusbar.pack(side=tk.BOTTOM, fill=tk.X)
+
+
+def main():
+    root = tk.Tk()
+    app = MainApp(root)
+    app.pack(side="top", fill="both", expand=True)
+    app.statusbar.set(text="Program has loaded.")
+    root.mainloop()
+
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    MainApp(root).pack(side="top", fill="both", expand=True)
-    root.mainloop()
+    main()
