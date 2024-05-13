ce3104e591115fbc18c52a564cc1618796166350
Martin Neloe <Martin.A.Neloe@student.edu.au>
2024-05-13 22:23:50 +1000

    Add graph frame to app and make 1st chart dynamic

--- app.py
+++ app.py
@@ -1,13 +1,71 @@
 import tkinter as tk
 import datetime
+import os
 import io
 import re
 import requests
 import webbrowser
+import plotly.express as px
+import pandas as pd
 
 from tkinter import ttk
 from newsapi import News, TopHeadlines
 from PIL import ImageTk, Image
+
+names_dict = {'Business Insider': 4,
+ 'NPR': 5,
+ 'ESPN': 27,
+ 'Ars Technica': 1,
+ 'The Athletic': 1,
+ 'CNET': 1,
+ 'ABC News': 1,
+ 'ReadWrite': 3,
+ 'MacRumors': 2,
+ 'Buzzfeed': 1,
+ 'Le Monde': 12,
+ 'Digital Trends': 7,
+ 'Android Police': 1,
+ '9to5Mac': 1,
+ 'HYPEBEAST': 4,
+ 'Rolling Stone': 1,
+ 'Yahoo Entertainment': 1,
+ 'Bleacher Report': 8,
+ 'Hipertextual': 1,
+ 'GameSpot': 1,
+ 'Die Zeit': 12,
+ 'Slashdot.org': 1,
+ 'BBC News': 1,
+ 'Presse-citron': 1}
+df = pd.DataFrame.from_dict(names_dict, orient="index").reset_index()
+df.columns = ["Source", "Count"]
+df.sort_values("Count", inplace=True)
+
+n = len(df)
+if n > 10:
+    top_10 = df.tail(10)
+    rest = df.head(n-10).copy()
+    rest["Source"] = "Other"
+    rest = rest.groupby("Source", as_index=False)["Count"].sum()
+    
+    CHART_DF = pd.concat([rest, top_10]).reset_index(drop=True)
+else:
+    CHART_DF = df
+
+cols = ["name",	"count",	"mean"]
+data = [
+["9to5Mac",	1,	2519.000000],
+["ABC News",	1,	2009.000000],
+["Android Police",	1,	6570.000000],
+["Ars Technica",	1,	5447.000000],
+["Bleacher Report",	7,	1770.571429],
+["Business Insider",	4,	3538.250000],
+["Buzzfeed",	1,	279.000000],
+["CNET",	1,	2155.000000],
+["Die Zeit",	12,	2868.000000],
+["Digital Trends",	7,	2943.571429]
+]
+OTHER_DF = pd.DataFrame(data, columns=cols)
+
 
 COUNTRY_OPTIONS = ['ae', 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de',
                    'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'in', 'it', 'jp', 'kr', 'lt', 
@@ -220,6 +278,133 @@ class articleGroup:
         self.timestamp_label.grid(row=self.row+2, column=3, sticky='nsew')
 
 
+class GraphFrame():
+    def __init__(self, root, bg_color, text_color, data):
+        self.root = root
+        self.bg_color = bg_color
+        self.text_color = text_color
+        self.stats = None
+        self.stats_count = None
+        self.stats_len = None
+        self.plots = {}
+        self.labels = {}
+
+        # Create subfolder if required
+        if not os.path.exists("images"):
+            os.mkdir("images")
+        else:
+            # Clear existing files 
+            for f in os.listdir("images"):
+                os.remove(f"images/{f}")
+        
+        # Data wrangling
+        self.summarise_by_source(data)
+
+        # Create plots and save pngs
+        self.create_count_plot()
+        self.create_len_plot()
+
+        # Display plots
+        filenames = ["fig_source_count.png", "fig_source_length.png", "fig_source_count.png", "fig_source_length.png"]
+        for i, filename in enumerate(filenames):
+            self.display_bar_plot(filename, counter=i)
+
+    def summarise_by_source(self, data):
+        stats = []
+        for article in data:
+            # Get source name and article length
+            source = article["source"]["name"]
+            length = self.get_article_length(article['content'])
+            published = article["publishedAt"]
+
+            stats.append([source, length, published])
+
+        df = pd.DataFrame(stats, columns=["Source", "Length", "Published"])
+        self.stats = df
+
+        count_df = df.groupby("Source", as_index=False)["Length"].count()
+        count_df.columns = ["Source", "Count"]
+        count_df.sort_values("Source", inplace=True)
+        count_df["Rank"] = count_df["Count"].rank(method="first")
+        count_df["Top"] = True
+        count_df.loc[count_df["Rank"] >= 10, "Top"] = False
+
+        count_df.loc[~count_df["Top"], "Source"] = "Other"
+        count_df = count_df.groupby("Source", as_index=False)["Count"].sum()
+        count_df.sort_values("Count", inplace=True)
+        self.stats_count = count_df
+
+    def get_article_length(self, content):
+        # Find char counter at end of content
+        pattern = r"\[\+\d+\s+\w+\]"
+
+        p = re.compile(pattern)
+        match = p.search(content)
+
+        if match:
+            # Extract number from char counter
+            extra_tag = match.group(0)
+            extra_count = re.findall(r"\d+", extra_tag)
+            extra_count = int(extra_count[0])
+
+            # Count string portion of content
+            stripped_content = content.replace(extra_tag, "")    
+            stripped_content = re.sub(r"\u2026", '', stripped_content)
+            content_count = len(stripped_content)
+
+        else:
+            extra_count = 0
+            content_count = len(content)
+        
+        return content_count + extra_count
+
+    def create_count_plot(self):
+        if self.stats_count is not None:
+            df = self.stats_count
+
+            colors = ["#018ADA", "#A5A4AC"]
+            df["Color"] = colors[0]
+            df.loc[df["Source"] == "Other", "Color"] = colors[1]
+
+            fig = px.bar(df, x="Count", y="Source", color="Color", orientation="h", 
+                        color_discrete_sequence=colors,
+                        width=400, height=300, template="plotly_dark")
+            fig.update_layout(showlegend=False, margin=dict(l=20, r=20, t=20, b=20))
+            fig.write_image("images/fig_source_count.png")
+
+    def create_len_plot(self):
+        self.length_df = OTHER_DF
+
+        if self.length_df is not None:
+            colors = ["#A5A4AC", "#018ADA"]
+            self.length_df["Color"] = colors[0]
+            self.length_df.loc[self.length_df["name"] == "9to5Mac", "Color"] = colors[1]
+
+            fig = px.bar(
+                self.length_df, x='mean', y='name', color="Color", orientation='h', 
+                color_discrete_sequence=colors,
+                width=400, height=300,
+                labels={'name': 'Source', 'mean': 'Length (avg.)'}, 
+                template='plotly_dark'
+            )
+            fig.add_vline(x=self.length_df["mean"].mean(), line_color="#9D0620", line_dash="dot")
+            fig.update_layout(showlegend=False, margin=dict(l=20, r=20, t=20, b=20))
+            fig.write_image("images/fig_source_length.png")
+
+    def display_bar_plot(self, filename, counter):
+        if os.path.isfile(f"images/{filename}"):
+            # Load image
+            img = Image.open(f"images/{filename}")
+            self.plots[counter] = ImageTk.PhotoImage(img)
+
+            # GUI img label
+            row = counter // 2
+            col = counter % 2
+            self.labels[counter] = tk.Label(self.root, image=self.plots[counter], bg=self.bg_color)
+            self.labels[counter].grid(row=row, column=col, sticky='nsew')
+            self.labels[counter].image = self.plots[counter]
+
+
 class ContentFrame(tk.Frame):
     def __init__(self, root, bg_color, text_color):
         self.root = root
@@ -369,7 +554,14 @@ class ContentFrame(tk.Frame):
 
             # Create or show new content
             if self.analytics_content is None:
-                self.analytics_content = tk.Label(self.root, text="TESTING")
+                self.analytics_content = tk.Frame()
+                self.analytics_frame = GraphFrame(
+                    self.analytics_content, 
+                    bg_color="#FFF1C8", 
+                    text_color="#000000",
+                    data=TEST_ARTICLES
+                )
+            
             self.analytics_content.pack(in_=self.display)
 
         else:
@@ -524,6 +716,21 @@ class MainApp:
 
                 #self.display[f"article_{count}"].config(image=image, width=image.width(), height=image.height())
                 count += 1
+
+import plotly.express as px
+import os
+
+
+def create_bar_plot(df):
+    if not os.path.exists("images"):
+        os.mkdir("images")
+    
+    fig = px.bar(df, x="Count", y="Source", orientation="h", template="plotly_dark")
+    fig.write_image("images/fig_source_count.png")
+
+def display_bar_plot():
+    if os.path.isfile("images/fig_source_count.png"):
+       pass
 
 def main():
     root = tk.Tk()
